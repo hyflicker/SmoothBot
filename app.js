2eebded08e0dc36d0a76e30b4a860e10e18444c4
hyflicker <hyflicker@gmail.com>
2022-07-12 08:30:42 -0400

    1.0.1 - Commit -added in an error handling system to be able to catch issues with the bot.

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const mysql = require('mysql2');
 const client = new Discord.Client({ intents: [Discord.Intents.FLAGS.GUILDS, Discord.Intents.FLAGS.GUILD_MESSAGES, Discord.Intents.FLAGS.GUILD_MESSAGE_REACTIONS],
     partials: ['MESSAGE','CHANNEL','REACTION']
 });
+const fs = require('fs')
 
 require('dotenv').config();
 
@@ -38,6 +39,16 @@ function handleDisconnect() {
 
 handleDisconnect();
 
+const errorHandler = function (error){
+    try {
+        let timestamp = new Date();
+        let data = `${timestamp}: Error - ${error} \n`
+        fs.appendFileSync('./logs/error.log',data)
+    } catch (e) {
+        console.log('ERROR: ',e)
+    }
+}
+
 client.on('ready', () => {
     console.log(`${client.user.tag} is Online!`);
 })
@@ -50,12 +61,13 @@ client.on('messageReactionAdd', async (reaction,user) =>{
             try {
                 await reaction.fetch();
                 db.execute(`SELECT * FROM reactionroles WHERE reactionName = '${reaction.emoji.name}' AND messageId = ${reaction.message.id}`, (err, results) => {
+                    if(err){errorHandler(err); return};
                     if(results.length > 0){
                         reaction.message.guild.members.cache.get(user.id).roles.add(`${results[0].roleId}`)
                     }
                 })
             } catch (error) {
-                console.error('Error',error)
+                errorHandler(error)
                 return
             }
 
@@ -69,12 +81,13 @@ client.on('messageReactionRemove', async (reaction,user) =>{
         try {
             await reaction.fetch();
             db.execute(`SELECT * FROM reactionroles WHERE reactionName = '${reaction.emoji.name}' AND messageId = ${reaction.message.id}`, (err, results) => {
+                if(err){errorHandler(err); return};
                 if(results.length > 0){
                     reaction.message.guild.members.cache.get(user.id).roles.remove(`${results[0].roleId}`)
                 }
             })
         } catch (error) {
-            console.error('Error',error)
+            errorHandler(error)
             return
         }
     }
@@ -85,24 +98,23 @@ client.on('interactionCreate', async interaction => {
     switch (interaction.commandName) {
         case "code":
             db.execute(`SELECT * FROM commandresponses WHERE commandName = '${interaction.commandName}' AND guildId = ${interaction.guildId}`, async (err,res) => {
-                if(err)console.log(err);
+                if(err){errorHandler(err); return};
                 if(res.length > 0){
                     await interaction.reply({content:`The code is ${res[0].commandResponse}!`, ephemeral: true})
                 }else{
                     await interaction.reply({content: `Error! Please try again later or have a mod to create a code.`})
                 }
-                
             })
             break;
         case "codechange":
             let newCode = interaction.options['_hoistedOptions'][0].value;
             db.execute(`UPDATE commandresponses SET commandResponse = '${newCode}' WHERE commandName = 'code' AND guildId = ${interaction.guildId}`, async (err, res)=>{
-                if(err)console.error;
+                if(err){errorHandler(err.sqlMessage); return};
                 if(res.affectedRows > 0){
                     await interaction.reply({content: `Code is now updated to ${newCode}`, ephemeral: true})
                 }else{
                     db.execute(`INSERT INTO commandresponses (commandName,commandResponse,guildId) VALUES ('code','${newCode}','${interaction.guildId}')`, async (err,results) => {
-                        if(err)console.log(err);
+                        if(err)errorHandler(err);
                         if(results.affectedRows > 0){
                             await interaction.reply({content: `Code is now updated to ${newCode}`, ephemeral: true});
                         }else{

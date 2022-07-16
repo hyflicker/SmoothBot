e704d3553150fedd582a78e8b0881f8bc8e0bcfb
hyflicker <hyflicker@gmail.com>
2022-07-16 13:28:18 -0400

    1.0.3 Update
    -Turned database calls into promises
    -added instant wave reaction for Docs welcome channel.

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 require('./slashBuilder')
 const Discord = require('discord.js');
 const mysql = require('mysql2');
-const client = new Discord.Client({ intents: [Discord.Intents.FLAGS.GUILDS, Discord.Intents.FLAGS.GUILD_MESSAGES, Discord.Intents.FLAGS.GUILD_MESSAGE_REACTIONS],
+const client = new Discord.Client({ intents: [Discord.Intents.FLAGS.GUILDS, Discord.Intents.FLAGS.GUILD_MESSAGES, Discord.Intents.FLAGS.GUILD_MESSAGE_REACTIONS, Discord.Intents.FLAGS.GUILD_MEMBERS],
     partials: ['MESSAGE','CHANNEL','REACTION']
 });
 const fs = require('fs')
@@ -55,48 +55,37 @@ client.on('ready', () => {
 })
 
 client.on('messageReactionAdd', async (reaction,user) =>{
-    
     if(reaction.message.channelId === '904080384843714560'){
-        console.log(`${user.username} reactionAdd with ${reaction.emoji.name}`)
-        if(reaction.partial){
-            try {
-                await reaction.fetch();
-                db.execute(`SELECT * FROM reactionroles WHERE reactionName = '${reaction.emoji.name}' AND messageId = ${reaction.message.id}`, (err, results) => {
-                    if(err){
-                        errorHandler(err);
-                        return;
-                    }
-                    if(results.length > 0){
-                        reaction.message.guild.members.cache.get(user.id).roles.add(`${results[0].roleId}`)
-                    }
-                })
-            } catch (error) {
-                errorHandler(error)
-                return
+        await reaction.fetch();
+        db.promise().execute(`SELECT * FROM reactionroles WHERE reactionName = '${reaction.emoji.name}' AND messageId = ${reaction.message.id}`) 
+        .then(([results]) => {
+            if(results.length > 0){
+                let roleName = reaction.message.guild.roles.cache.filter(role => role.id === `${results[0].roleId}`).map((role) => role.name)[0];
+                reaction.message.guild.members.cache.get(user.id).roles.add(`${results[0].roleId}`)
+                console.log(`${user.username} reactionAdd with ${reaction.emoji.name} to get ${roleName} role`)
             }
-
-        }
+        })
+        .catch((err) =>{
+            errorHandler(err);
+                return;
+        })       
     }
 })
 
 client.on('messageReactionRemove', async (reaction,user) =>{
     if(reaction.message.channelId === '904080384843714560'){
-        console.log(`${user.username} reactionRemove with ${reaction.emoji.name}`)
-        try {
-            await reaction.fetch();
-            db.execute(`SELECT * FROM reactionroles WHERE reactionName = '${reaction.emoji.name}' AND messageId = ${reaction.message.id}`, (err, results) => {
-                if(err){
-                    errorHandler(err);
-                    return;
-                }
-                if(results.length > 0){
-                    reaction.message.guild.members.cache.get(user.id).roles.remove(`${results[0].roleId}`)
-                }
-            })
-        } catch (error) {
-            errorHandler(error)
-            return
-        }
+        await reaction.fetch();
+        db.promise().execute(`SELECT * FROM reactionroles WHERE reactionName = '${reaction.emoji.name}' AND messageId = ${reaction.message.id}`)
+        .then(([results]) => {
+            if(results.length > 0){
+                let roleName = reaction.message.guild.roles.cache.filter(role => role.id === `${results[0].roleId}`).map((role) => role.name)[0];
+                reaction.message.guild.members.cache.get(user.id).roles.remove(`${results[0].roleId}`)
+                console.log(`${user.username} reactionRemove with ${reaction.emoji.name} to get rid of the ${roleName} role`)
+            }
+        })
+        .catch((err) => {
+            errorHandler(err)
+        })   
     }
 })
 
@@ -104,35 +93,63 @@ client.on('interactionCreate', async interaction => {
     if(!interaction.isCommand()) return;
     switch (interaction.commandName) {
         case "code":
-            db.execute(`SELECT * FROM commandresponses WHERE commandName = '${interaction.commandName}' AND guildId = ${interaction.guildId}`, async (err,res) => {
-                if(err){errorHandler(err); return};
+            db.promise().execute(`SELECT * FROM commandresponses WHERE commandName = '${interaction.commandName}' AND guildId = ${interaction.guildId}`)
+            .then( async([res]) => {
                 if(res.length > 0){
                     await interaction.reply({content:`The code is ${res[0].commandResponse}!`, ephemeral: true})
                 }else{
                     await interaction.reply({content: `Error! Please try again later or have a mod to create a code.`})
                 }
             })
+            .catch((err) =>{
+                errorHandler(err);
+                return
+            })
             break;
         case "codechange":
             let newCode = interaction.options['_hoistedOptions'][0].value;
-            db.execute(`UPDATE commandresponses SET commandResponse = '${newCode}' WHERE commandName = 'code' AND guildId = ${interaction.guildId}`, async (err, res)=>{
-                if(err){errorHandler(err.sqlMessage); return};
+            db.promise().execute(`UPDATE commandresponses SET commandResponse = '${newCode}' WHERE commandName = 'code' AND guildId = ${interaction.guildId}`)
+            .then( async([res]) => {
                 if(res.affectedRows > 0){
                     await interaction.reply({content: `Code is now updated to ${newCode}`, ephemeral: true})
                 }else{
-                    db.execute(`INSERT INTO commandresponses (commandName,commandResponse,guildId) VALUES ('code','${newCode}','${interaction.guildId}')`, async (err,results) => {
-                        if(err)errorHandler(err);
+                    db.promise().execute(`INSERT INTO commandresponses (commandName,commandResponse,guildId) VALUES ('code','${newCode}','${interaction.guildId}')`)
+                    .then(async(results) => {
                         if(results.affectedRows > 0){
                             await interaction.reply({content: `Code is now updated to ${newCode}`, ephemeral: true});
                         }else{
                             await interaction.reply({content: `Error! Please try again later.`})
                         }
                     })
+                    .catch((err) => {
+                        errorHandler(err.sqlMessage);
+                        return;
+                    })    
                 }
+            })
+            .catch((err) => {
+                errorHandler(err.sqlMessage);
+                return;
             })
             break;
         default:
             break;
+    }
+})
+
+client.on('guildMemberAdd', (member) => {
+    try {
+        if(member.guildId === '839568492113297498'){
+            setTimeout(() => {
+                // const message = member.guild.channels.cache.get('993846343489564764').lastMessage
+                const message = member.guild.channels.cache.get('866783039702827078').lastMessage
+                let emoji = member.guild.emojis.cache.filter(emote => emote.name === 'doctorrSup').map((emote) => emote)[0]
+                message.react(`${emoji.id}`)
+                .catch(console.error)
+            }, 500)
+        }   
+    } catch (error) {
+        errorHandler(error)
     }
 })
 
